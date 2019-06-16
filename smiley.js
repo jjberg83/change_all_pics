46b89c7d4c1ca0eedf47dfe771a79cc3410c3ad7
Jørund Jenssen Berg <jjberg@gmail.com>
2019-06-16 08:57:49 +0200

    Initial commit - clean website with smileys

---
+++ smiley.js
@@ -0,0 +1,18 @@
+let smileyBtn = document.getElementById('toSmiley');
+let angryBtn = document.getElementById('toAngry');
+
+function allSmiles(a) {
+  document.getElementById('indexPic').src = a;
+}
+
+function longFace(b) {
+  document.getElementById('indexPic').src = b;
+}
+
+smileyBtn.onclick = function () {
+  allSmiles("images/smiley_blue.png");
+}
+
+angryBtn.onclick = function () {
+  longFace("images/angry_blue.png");
+}
